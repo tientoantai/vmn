b29ff305516ad4bd6848c942d9ae30fa3fe8b0ce
Tien Toan Tai <nguyenminhtien61vn@gmail.com>
2016-02-29 11:20:37 +0700

    remedies table seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(MedicinalPlant::class);
+        $this->call(MemberTableSeeder::class);
+        $this->call(RemedyTableSeeder::class);
+        $this->call(remedyIngredientsSeeder::class);
+        $this->call(remedyIngredientsSeeder::class);
     }
 }
--- database/seeds/RemedyTableSeeder.php
+++ database/seeds/RemedyTableSeeder.php
@@ -11,18 +11,14 @@ class RemedyTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('medicinal_plants')->truncate();
+        DB::table('remedies')->truncate();
 
         $remedy = new \VMN\Article\Remedy();
-        $remedy->title          = 'Bài thuốc chữa viêm đại tràng từ nghệ và ngải cứu';
-        $remedy->ingredients    = '1- Mật lợn tươi: 1 cái (lợn có trọng lượng từ 70kg đến 100kg).
-                                    2- Nghệ vàng tươi: 02 lạng
-                                    3- Mật ong: 30ml
-                                    4- Ngải cứu tươi: 5 bó to ( Tương đương với khoảng 500g).';
-        $remedy->description    = 'Nghệ tươi và ngải cứu rửa sạch, cắt nhỏ cho vào máy xay sinh tố xay cùng với 0,5 lít nước thật nhuyễn. Sau đó lọc qua vải phin lấy nước. Bã bỏ đi. Mật lợn lọc lấy nước (Vì mật lợn hay có sạn sỏi bên trong). Cho tất cả phần nước hỗn hợp nghệ + ngải cứu + nước mật lợn+ mật ong vào môt nồi, quấy đều, đun nhỏ lửa để cô lại thành cao. Cho phần cao đó vào một cái lọ bảo quản trong ngăn mát tủ lạnh để sử dụng dần.';
-        $remedy->note           = 'Để tránh mất thời gian và đủ lượng dùng, ta nên làm một lần với tỷ lệ trên cho 4 hoặc 5 cái mật lợn.';
-        $remedy->usage          = 'chữa viêm đại tràng';
-        $remedy->utility        = 'Mỗi ngày 2 lần: Sáng và tối, uống vào trước bữa ăn 30 phút. Liều lượng mỗi lần 1 viên to như hạt lạc là đủ.';
+        $remedy->title          = 'Bài thuốc giúp an thai từ ngải cứu';
+        $remedy->description    = 'nếu thấy có hiện tượng đau bụng, ra máu, dùng 16gr lá ngải cứu, 16gr lá tía tô, sắc cùng với 600ml nước, sắc còn 100ml.';
+        $remedy->note           = 'Ngải cứu không có tác dụng kích thích với tử cung có thai nên không gây sảy thai.';
+        $remedy->usage          = 'An thai';
+        $remedy->utility        = 'Chia làm 3-4 lần uống/ngày';
         $remedy->ratingPoint    = 3;
         $remedy->thumbnailUrl   = '1.jpg';
         $remedy->imgUrl         = json_encode(['1.jpg','2.jpg']);
