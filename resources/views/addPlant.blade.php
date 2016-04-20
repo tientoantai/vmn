9469236f8c13305b540b0e6bc539d1d9ea2cd5ac
Tien Toan Tai <nguyenminhtien61vn@gmail.com>
2016-04-20 15:22:19 +0700

    fix bug add plant

--- resources/views/addPlant.blade.php
+++ resources/views/addPlant.blade.php
@@ -121,12 +121,14 @@
                 var $createPlant = $.post($(this).attr('action'), plantRaw);
 
                 $createPlant.then(function (response) {
-                    var msg = ''
-                    $.each(response.message, function(key, value){
-                        msg += value + '\n';
-                    });
-                    alert (msg);
-                    if (response.status != 'error'){
+                    if(response.status == 'error'){
+                        var msg = '';
+                        $.each(response.message, function(key, value){
+                            msg += value + '\n';
+                        });
+                        alert (msg);
+                    }else{
+                        alert (response.message);
                         window.location.href = '/medicinalPlants'
                     }
                 });
