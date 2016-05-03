1896ef44fff4ec105e9a387378173c89d1a936eb
Tien Toan Tai <nguyenminhtien61vn@gmail.com>
2016-05-03 14:26:21 +0700

    route to online server

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,8 +28,8 @@ use App\Http\Middleware\ProfileMiddleWare;
 */
 
 
-Route::group(['domain' => 'admin.vmn.local', 'middleware' => ['web']], function () {
-//Route::group(['domain' => 'admin.vmn.vnvalley.com', 'middleware' => ['web']],function () {
+//Route::group(['domain' => 'admin.vmn.local', 'middleware' => ['web']], function () {
+Route::group(['domain' => 'admin.vmn.vnvalley.com', 'middleware' => ['web']],function () {
     Route::get('/', function(){
        return redirect('managementLogin');
     });
