337539fac816c4e8b5171dcad66b417af7346317
Tien Toan Tai <nguyenminhtien61vn@gmail.com>
2016-05-03 16:53:14 +0700

    update profile add field gender

--- app/Http/Middleware/ProfileMiddleWare.php
+++ app/Http/Middleware/ProfileMiddleWare.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use VMN\Auth\Member;
 
 class ProfileMiddleWare
 {
     public function handle(Request $request, \Closure $next)
     {
+        if ($request->get('credential') != \Session::get('credential')['attributes']['name'])
+        {
+            return response()->json([
+                'message' => 'Bạn không thể thực hiện yêu cầu này!'
+            ]);
+        }
         $member = $this->makeMember($request);
         app()->bind(get_class($member), function () use ($member) {
             return $member;
@@ -21,6 +28,7 @@ class ProfileMiddleWare
         $member->firstName = $request->get('firstName');
         $member->lastName = $request->get('lastName');
         $member->DoB = $request->get('DoB');
+        $member->gender = $request->get('gender');
         return $member;
     }
 
