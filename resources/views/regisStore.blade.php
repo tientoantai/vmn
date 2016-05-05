f4799bd8705293d13d21f263fba18aea858c1aee
Tien Toan Tai <nguyenminhtien61vn@gmail.com>
2016-05-05 09:02:14 +0700

    DoB can not before today

--- resources/views/regisStore.blade.php
+++ resources/views/regisStore.blade.php
@@ -54,7 +54,9 @@
 <!-- Javascript -->
 <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
 <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<script>
 
+</script>
 </body>
 
 </html>
